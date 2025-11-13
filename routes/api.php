d3267d450932dd4f940f21590035406433348fe0
mauro <mauro.emc@outlook.com>
2025-11-13 14:27:07 -0300

    Inclusão das rotas do Travel Domain

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Domains\Auth\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Domains\Auth\Http\Controllers\AuthController;
 
+Route::prefix('travel')->group(base_path('app/Domains/Travel/routes/travel.php'));
+
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
 Route::middleware('jwt')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
